d69efddbeb5b98a22ee07f8e37ecfd74dfc27d2a
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2023-02-10 11:38:41 +0530

    matlabevent

---
+++ harsh02.m
@@ -0,0 +1,7 @@
+x=input('Enter the vaue of x =   ');
+if x<40
+    y=2*x;
+else
+    y=5*x;
+end
+y
---
+++ harsh05.m
@@ -0,0 +1,9 @@
+x1=input('Enter the value of x1=');
+x2=input('Enter the value of x2=');
+w1=1;w2=1;
+net=w1*x1+w2*x2;
+    if net<2
+        y=0
+    else
+        y=1
+    end
